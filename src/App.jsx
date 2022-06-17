fbe07ba471cf7122339943be05319a2bb1aa9ffe
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-06-17 13:08:58 -0300

    creando los componentes header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+/* Libraries */
+import { Routes, Route } from "react-router-dom"
+
+/* Components */
 import Listado from "./components/Listado";
 import Login from "./components/Login";
-import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+/* Styles */
+
 
 function App() {
     return (
         <>
+            <Header />
+
             <Routes>
                 <Route path="/" element={ <Login /> }/>
                 <Route path="/listado" element={ <Listado /> }/>
             </Routes>
+
+            <Footer />
         </>
     );
 }
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Listado = () => {
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+      token === null && navigate('/');
+  });
+
+  
   return (
     <h2>listado</h2>
   )
